150d3fbc121ff6e03a36ba4b0530eae29ee5da78
Jack <jackli2468@gmail.com>
2020-01-11 15:21:17 +0800

    Create LoginStyles

---
+++ src/screens/Login/LoginStyles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export const LoginStyles = StyleSheet.create({
+    cardView: {
+        width: '80%',
+        padding: 10,
+        marginTop: 20
+    }
+});
