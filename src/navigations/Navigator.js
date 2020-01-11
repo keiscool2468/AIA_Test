bff8b8357c7f52c4f53b3f807ed2939ce674584f
Jack <jackli2468@gmail.com>
2020-01-11 21:19:32 +0800

    Create AppNavigation

--- src/navigations/Navigator.js
+++ src/navigations/Navigator.js
@@ -2,12 +2,14 @@ import React, {useEffect} from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer  } from 'react-navigation';
 import AuthNavigation from './AuthNavigation';
+import AppNavigation from './AppNavigation';
 
 const Navigator = (props) => {
   	return (
     	createStackNavigator(
         	{
-				Auth: AuthNavigation
+				Auth: AuthNavigation,
+				App: AppNavigation
 			},
 			{
 				initialRouteName: 'Auth',
