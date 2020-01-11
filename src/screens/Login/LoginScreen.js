a6dcd68d1e2be51d1992a1c90f59c9a35f072e7a
Jack <jackli2468@gmail.com>
2020-01-11 15:21:02 +0800

    Create LoginScreen

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import {
-  SafeAreaView,
+  TextInput,
+  View,
+  Text
 } from 'react-native';
+import {LoginStyles} from './LoginStyles';
+import {GeneralStyles} from '../../styles/GeneralStyles';
 
+const LoginScreen = () => {
 
-const HomeScreen = () => {
+    const onChangeText = (value, key) => {
+
+    }
+
     return (
-      	<>
-        	<SafeAreaView>
-        	</SafeAreaView>
-      	</>
+        <View style={[GeneralStyles.container]}>
+            <Text style={[GeneralStyles.title]}>
+                AIA Testing
+            </Text>
+
+            <View style={[GeneralStyles.cardView, LoginStyles.cardView]}>
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={(text) => onChangeText(text, 'account')}
+                />
+                <TextInput
+                    secureTextEntry={true}
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={(text) => onChangeText(text, 'password')}
+                />
+            </View>
+        </View>
     );
 };
 
-  export default HomeScreen;
+export default LoginScreen;
