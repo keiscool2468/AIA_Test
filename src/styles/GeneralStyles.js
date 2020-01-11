722d70fba7066cd593a3072ee5ac0ca8e967a055
Jack <jackli2468@gmail.com>
2020-01-11 15:12:38 +0800

    Create container, title, cardView style

---
+++ src/styles/GeneralStyles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+export const GeneralStyles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 20,
+    },
+    cardView: {
+        shadowOffset:{  width: 2,  height: 2,  },
+        shadowColor: 'rgba(0,0,0,0.2)',
+        shadowOpacity: 1.0,
+        borderRadius: 4,
+        backgroundColor: `#fff`,
+    }
+});
