48e1e42dab92d65f9255c429c592aa345e32274e
Jack <jackli2468@gmail.com>
2020-01-12 22:12:47 +0800

    Create water Styles

---
+++ src/screens/EditWater/EditWaterStyles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export const EditWaterStyles = StyleSheet.create({
+    cardContainer: {
+        flexDirection: 'row',
+        width: '80%',
+        marginTop: 20
+    },
+    cardView: {
+        flex: 1,
+        marginHorizontal: 10,
+        padding: 10,
+        alignItems: 'center'
+    }
+});
